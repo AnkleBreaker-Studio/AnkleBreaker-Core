485320875616711239388875ad695279c81ff4d5
XuX26 <ulysse.raillon@hotmail.fr>
2026-03-03 00:03:52 +0100

    [FIX] asmdef, v1.0.3 removed old Utils GUID refs breaking compat with old package: restore both old Utils GUID refs and new Utils Inspector name ref [FEAT] AnkleBreakerCoreDependenciesInstaller, auto-install replaced with EditorWindow prompting user to install Utils Inspector when AB_UTILS_INSPECTOR is not defined

--- Editor/AnkleBreakerCoreDependenciesInstaller.cs
+++ Editor/AnkleBreakerCoreDependenciesInstaller.cs
@@ -9,24 +9,31 @@ namespace AnkleBreaker.Core.Editor
 {
     public class AnkleBreakerCoreDependenciesInstaller
     {
+        private const string UTILS_INSPECTOR_URL = "https://github.com/AnkleBreaker-Studio/AnkleBreaker-Utils-Inspector.git#Release";
+        private const string UTILS_INSPECTOR_DISPLAY_NAME = "AnkleBreaker.Utils.Inspector";
+        public const string DISMISSED_KEY = "AB_UtilsInspector_Dismissed";
+
         [InitializeOnLoadMethod]
         public static void CheckAllDependencies()
         {
 #if !AB_UTILS_INSPECTOR
-            StartCoroutine(InstallPackageAsync(
-                "https://github.com/AnkleBreaker-Studio/utils-inspector.git#Release",
-                "AB_UTILS_INSPECTOR-Install",
-                "AnkleBreaker.Utils.Inspector"));
+            if (!SessionState.GetBool(DISMISSED_KEY, false))
+            {
+                EditorApplication.delayCall += () =>
+                {
+                    AnkleBreakerDependencyWindow.ShowWindow();
+                };
+            }
 #endif
         }
 
-        [MenuItem("Help/AnkleBreaker/Core/Update All Requirements (Package Manager)", priority = 0)]
-        public static void InstallRequirements()
+        [MenuItem("Help/AnkleBreaker/Core/Install Utils Inspector Package", priority = 0)]
+        public static void InstallUtilsInspector()
         {
             StartCoroutine(InstallPackageAsync(
-                "https://github.com/AnkleBreaker-Studio/utils-inspector.git#Release",
+                UTILS_INSPECTOR_URL,
                 "AB_UTILS_INSPECTOR-Install",
-                "AnkleBreaker.Utils.Inspector"));
+                UTILS_INSPECTOR_DISPLAY_NAME));
         }
 
         [MenuItem("Help/AnkleBreaker/Core/Documentation", priority = 3)]
@@ -41,7 +48,7 @@ namespace AnkleBreaker.Core.Editor
             //Application.OpenURL("https://discord.gg/ANKLEBREAKERASSETSUPPORT");
         }
 
-        private static IEnumerator InstallPackageAsync(string packageUrl, string sessionKey, string displayName)
+        public static IEnumerator InstallPackageAsync(string packageUrl, string sessionKey, string displayName)
         {
             yield return null;
 
@@ -69,7 +76,7 @@ namespace AnkleBreaker.Core.Editor
         #region Editor Coroutine System
         private static List<IEnumerator> _coroutines;
 
-        private static void StartCoroutine(IEnumerator handle)
+        public static void StartCoroutine(IEnumerator handle)
         {
             if (_coroutines == null)
             {
@@ -103,5 +110,120 @@ namespace AnkleBreaker.Core.Editor
             }
         }
         #endregion
+    }
+
+    public class AnkleBreakerDependencyWindow : EditorWindow
+    {
+        private const string UTILS_INSPECTOR_URL = "https://github.com/AnkleBreaker-Studio/AnkleBreaker-Utils-Inspector.git#Release";
+        private const string UTILS_INSPECTOR_DISPLAY_NAME = "AnkleBreaker.Utils.Inspector";
+
+        private static GUIStyle _titleStyle;
+        private static GUIStyle _messageStyle;
+        private static GUIStyle _warningStyle;
+
+        public static void ShowWindow()
+        {
+            var window = GetWindow<AnkleBreakerDependencyWindow>(true, "AnkleBreaker Core - Missing Dependency", true);
+            window.minSize = new Vector2(500, 280);
+            window.maxSize = new Vector2(500, 280);
+            window.ShowUtility();
+        }
+
+        private void OnGUI()
+        {
+            InitStyles();
+
+            EditorGUILayout.Space(15);
+
+            EditorGUILayout.LabelField("Missing Recommended Package", _titleStyle);
+
+            EditorGUILayout.Space(10);
+
+            EditorGUILayout.LabelField(
+                "AnkleBreaker Core strongly recommends installing the Utils Inspector package.\n\n" +
+                "Without it, some features will be disabled:\n" +
+                "  - Custom inspector attributes (HideInNormalInspector, Button, etc.)\n" +
+                "  - Enhanced editor tooling for AnkleBreaker components",
+                _messageStyle);
+
+            EditorGUILayout.Space(10);
+
+            EditorGUILayout.LabelField(
+                "It is highly recommended to install this package for the best experience.",
+                _warningStyle);
+
+            EditorGUILayout.Space(15);
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+
+            if (GUILayout.Button("Install Utils Inspector", GUILayout.Width(200), GUILayout.Height(35)))
+            {
+                AnkleBreakerCoreDependenciesInstaller.StartCoroutine(
+                    AnkleBreakerCoreDependenciesInstaller.InstallPackageAsync(
+                        UTILS_INSPECTOR_URL,
+                        "AB_UTILS_INSPECTOR-Install",
+                        UTILS_INSPECTOR_DISPLAY_NAME));
+                Close();
+            }
+
+            GUILayout.Space(10);
+
+            if (GUILayout.Button("Remind Me Later", GUILayout.Width(140), GUILayout.Height(35)))
+            {
+                Close();
+            }
+
+            GUILayout.FlexibleSpace();
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space(5);
+
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+
+            if (GUILayout.Button("Don't Show Again (this session)", GUILayout.Width(250), GUILayout.Height(22)))
+            {
+                SessionState.SetBool(AnkleBreakerCoreDependenciesInstaller.DISMISSED_KEY, true);
+                Close();
+            }
+
+            GUILayout.FlexibleSpace();
+            EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.Space(10);
+        }
+
+        private static void InitStyles()
+        {
+            if (_titleStyle == null)
+            {
+                _titleStyle = new GUIStyle(EditorStyles.boldLabel)
+                {
+                    fontSize = 16,
+                    alignment = TextAnchor.MiddleCenter
+                };
+            }
+
+            if (_messageStyle == null)
+            {
+                _messageStyle = new GUIStyle(EditorStyles.wordWrappedLabel)
+                {
+                    fontSize = 12,
+                    padding = new RectOffset(15, 15, 0, 0)
+                };
+            }
+
+            if (_warningStyle == null)
+            {
+                _warningStyle = new GUIStyle(EditorStyles.wordWrappedLabel)
+                {
+                    fontSize = 12,
+                    fontStyle = FontStyle.Bold,
+                    alignment = TextAnchor.MiddleCenter
+                };
+                _warningStyle.normal.textColor = new Color(1f, 0.6f, 0f);
+            }
+        }
     }
 }
